77e7c67ab4571beabf2191e2d46214fbf178e5f2
Andreas Hager <andy@mazebert.com>
2019-11-09 09:59:01 +0100

    Add test

--- src/test/java/org/jusecase/inject/TrainerWithConstructorParamTest.java
+++ src/test/java/org/jusecase/inject/TrainerWithConstructorParamTest.java
@@ -3,17 +3,22 @@ package org.jusecase.inject;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TrainerWithConstructorParamTest implements ComponentTest {
     @Trainer private Gateway trainer = new Gateway("hello");
+    @Trainer(named = "name") private Gateway namedTrainer = new Gateway("hello named");
 
     @Test
     void injection() {
         Service service = new Service();
         assertThat(service.gateway).isSameAs(trainer);
         assertThat(trainer.source).isEqualTo("hello");
+
+        assertThat(service.namedGateway).isSameAs(namedTrainer);
+        assertThat(namedTrainer.source).isEqualTo("hello named");
     }
 
     public static class Gateway {
@@ -28,5 +33,9 @@ public class TrainerWithConstructorParamTest implements ComponentTest {
     public static class Service {
         @Inject
         public Gateway gateway;
+
+        @Inject
+        @Named("name")
+        public Gateway namedGateway;
     }
 }
